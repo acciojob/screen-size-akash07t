bc70842ccc7187f719be67269c9411ae8a5ac359
Akash Tiwari <124699923+akash07t@users.noreply.github.com>
2023-04-18 15:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const expectedWidth = window.innerWidth;
+const expectedHeight = window.innerHeight;
+const item = document.querySelector("#sizeInfo h1");
+addEventListener("resize",resize);
+function resize(){
+		item.innerHTML="Width: "+expectedWidth+" and Height: "+expectedHeight;
+};
+window.onload = resize;
+window.onresize = resize;
+
